51e5795a162240fd6e72e097f5f54a33c330b6d2
alexm <alex-m@outlook.com>
2023-08-31 14:21:05 -0700

    Adjust spacing and indentation

--- tree.js
+++ tree.js
@@ -1,35 +1,34 @@
-/** TreeNode: node for a general tree. */
 
+/** TreeNode: node for a general tree. */
 class TreeNode {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-  }
+    constructor(val, children = []) {
+        this.val = val;
+        this.children = children;
+    }
 }
+
 
 class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
+    constructor(root = null) {
+        this.root = root;
+    }
 
-  /** sumValues(): add up all of the values in the tree. */
+    /** sumValues(): add up all of the values in the tree. */
+    sumValues() {
 
-  sumValues() {
-    
-  }
+    }
 
-  /** countEvens(): count all of the nodes in the tree with even values. */
+    /** countEvens(): count all of the nodes in the tree with even values. */
+    countEvens() {
 
-  countEvens() {
+    }
 
-  }
+    /** numGreater(lowerBound): return a count of the number of nodes
+     * whose value is greater than lowerBound. */
+    numGreater(lowerBound) {
 
-  /** numGreater(lowerBound): return a count of the number of nodes
-   * whose value is greater than lowerBound. */
-
-  numGreater(lowerBound) {
-
-  }
+    }
 }
+
 
 module.exports = { Tree, TreeNode };
